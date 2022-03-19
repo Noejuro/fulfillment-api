4b4938abe68efeee3d10565ede4032cdcd89a3e3
Noé <noe_juarez@epam.com>
2022-03-19 02:20:31 -0600

    [INITIAL COMMIT]

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.port || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'))
+})
